90e9de1b27ac65fbdb7804f6d1936016625ed92a
Jessica Joseph <jessjeanjo@gmail.com>
2019-01-07 08:34:03 -0500

    fix missing data

--- 0_data_preprocessing/main.py
+++ 0_data_preprocessing/main.py
@@ -11,11 +11,17 @@ import numpy as npy
 import matplotlib.pyplot as pplot
 import pandas
 
+from sklearn.preprocessing import Imputer
+
 # Import dataset
 dataset = pandas.read_csv('Data.csv')
 mat_feat = dataset.iloc[:, :-2].values
 dep_vec = dataset.iloc[:, -2].values
 
+# Take care of missing data
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(mat_feat[:, 1:3])
+mat_feat[:, 1:3] = imputer.transform(mat_feat[:, 1:3])
 
 
 
